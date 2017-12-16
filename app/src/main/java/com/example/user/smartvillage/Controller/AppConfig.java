d82158ccc662978879d50403858a98bafbb872eb
firrananda <firrananda1407@gmail.com>
2017-12-16 17:07:53 +0700

    progres main

--- app/src/main/java/com/example/user/smartvillage/Controller/AppConfig.java
+++ app/src/main/java/com/example/user/smartvillage/Controller/AppConfig.java
@@ -1,7 +1,7 @@
 package com.example.user.smartvillage.Controller;
 
 public class AppConfig {
-    private static final String ROOT_URL = "http://192.168.43.164/APImasrapip/";
+    private static final String ROOT_URL = "http://192.168.43.164/APIsmartvillage/";
     public static final String URL_REGISTER = ROOT_URL + "register.php";
     public static final String URL_LOGIN= ROOT_URL + "login.php";
 }
